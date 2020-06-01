32a9206995c1bcbf0248995d5f3bb5609011f511
Giovanni Mormone <giovanni.mormone@studio.unibo.it>
2020-06-01 23:52:11 +0200

    added select terminali from zonaid in db

--- server/src/test/scala/TestTerminale.scala
+++ server/src/test/scala/TestTerminale.scala
@@ -6,14 +6,14 @@ import org.scalatest._
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
-trait Init{
+trait Init2{
   protected var terminale: List[Terminale] = _
 
   val result: Int = Await.result(runScript(),Duration.Inf)
   require(result==1)
 }
 
-class TestTerminale extends  AsyncFlatSpec with BeforeAndAfterEach with Init{
+class TestTerminale extends  AsyncFlatSpec with BeforeAndAfterEach with Init2{
 
   override def beforeEach(): Unit = {
     terminale = List(Terminale("Cansas",1,Some(1)),Terminale("Pajaritos",1,Some(5)))
@@ -23,5 +23,13 @@ class TestTerminale extends  AsyncFlatSpec with BeforeAndAfterEach with Init{
   it should "eventually return a list of terminali with length of 2 " in {
     val futureTerminali: Future[Option[List[Terminale]]] = TerminaleOperation.getTermininaliInZona(1)
     futureTerminali map {terminale => assert(terminale.head.length == 2)}
+  }
+  it should "eventually return a list of terminali equals to the provided list " in {
+    val futureTerminali: Future[Option[List[Terminale]]] = TerminaleOperation.getTermininaliInZona(1)
+    futureTerminali map {t => assert(t.head.equals(terminale))}
+  }
+  it should "eventually return a list of terminali with length of 1 " in {
+    val futureTerminali: Future[Option[List[Terminale]]] = TerminaleOperation.getTermininaliInZona(3)
+    futureTerminali map {terminale => assert(terminale.head.length == 1)}
   }
 }
