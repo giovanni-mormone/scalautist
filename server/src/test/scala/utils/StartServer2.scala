9222750650f8d324c42434150cb14bafa75e6430
fra.cassano2 <francesco.cassano2@studio.unibo.it>
2020-06-23 15:41:59 +0200

    init test

--- server/src/test/scala/utils/StartServer2.scala
+++ server/src/test/scala/utils/StartServer2.scala
@@ -4,6 +4,6 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 trait StartServer2 {
-  val result: Int = Await.result(DatabaseHelper().runScript2(),Duration.Inf)
+  val result: Int = Await.result(DatabaseHelper().runScrptDisp(),Duration.Inf) //Await.result(DatabaseHelper().runScript2(),Duration.Inf)
   require(result==1)
 }
