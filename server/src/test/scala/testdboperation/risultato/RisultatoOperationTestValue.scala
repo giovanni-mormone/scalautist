532397dc28ac6f0ebedb85af27cb74b446398a10
fra.cassano2 <francesco.cassano2@studio.unibo.it>
2020-06-24 10:26:56 +0200

    some fix

--- server/src/test/scala/testdboperation/risultato/RisultatoOperationTestValue.scala
+++ server/src/test/scala/testdboperation/risultato/RisultatoOperationTestValue.scala
@@ -3,7 +3,7 @@ package testdboperation.risultato
 import java.sql.Date
 
 object RisultatoOperationTestValue {
-  val idAutist: Int = 17
+  val idAutist: Int = 6
   val idManager: Int = 1
   val idNobody: Int = 9999
   val date: Date = Date.valueOf("2020-06-15")
