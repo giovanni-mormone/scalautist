1ebdb4f8c2e75d7877feca36d9b54cce0656d06f
fra.cassano2 <francesco.cassano2@studio.unibo.it>
2020-06-26 16:48:18 +0200

    test disponibilita server

--- server/src/test/scala/testhttp/TestHttpDisponibilita.scala
+++ server/src/test/scala/testhttp/TestHttpDisponibilita.scala
@@ -15,6 +15,9 @@ object TestHttpDisponibilita {
   private val getAvailability: (String, Request[(Int, Dates)]) = ("/getdisponibilitainweek", Request(None))
   private val setAvailability: (String, Request[(Disponibilita, Id)]) = ("/setdisponibilita", Request(None))
   private val getExtraAvailability: (String, Request[(Int, Int, Int)]) = ("/extraavailability", Request(None))
+  private val getExtraAvailability_Error1: (String, Request[(Int, Int, Int)]) = ("/extraavailability", Request(Some((9999, 1, 1))))
+  private val getExtraAvailability_Error2: (String, Request[(Int, Int, Int)]) = ("/extraavailability", Request(Some((1, 9999, 1))))
+  private val getExtraAvailability_Error3: (String, Request[(Int, Int, Int)]) = ("/extraavailability", Request(Some((1, 1, 9999))))
 }
 
 class TestHttpDisponibilita extends AnyWordSpec with ScalatestRouteTest with StartServer {
@@ -37,6 +40,24 @@ class TestHttpDisponibilita extends AnyWordSpec with ScalatestRouteTest with Sta
     "return a bad request for extraAvailability if a bad request is sent" in {
       Post(getExtraAvailability._1, getExtraAvailability._2) ~> routeDisponibilita ~> check {
         responseAs[Response[Int]].payload.contains(statusCodes.BAD_REQUEST)
+      }
+    }
+
+    "return a error 1 for extraAvailability if wrong idRisultato is sent" in {
+      Post(getExtraAvailability_Error1._1, getExtraAvailability_Error1._2) ~> routeDisponibilita ~> check {
+        responseAs[Response[Int]].payload.contains(statusCodes.ERROR_CODE1)
+      }
+    }
+
+    "return a error 2 for extraAvailability if wrong idTerminale is sent" in {
+      Post(getExtraAvailability_Error2._1, getExtraAvailability_Error2._2) ~> routeDisponibilita ~> check {
+        responseAs[Response[Int]].payload.contains(statusCodes.ERROR_CODE2)
+      }
+    }
+
+    "return a error 3 for extraAvailability if wrong idTurno is sent" in {
+      Post(getExtraAvailability_Error3._1, getExtraAvailability_Error3._2) ~> routeDisponibilita ~> check {
+        responseAs[Response[Int]].payload.contains(statusCodes.ERROR_CODE3)
       }
     }
 
