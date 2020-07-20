3a34730942024305546b787b5006c6547e9cd65f
faspeeencina <fabian.aspeeencina@studio.unibo.it>
2020-07-20 12:07:04 +0200

    persistence rabbitMQ ok and create queue that receive information from your creation

--- event/src/main/scala/persistence/ConfigEmitterPersistence.scala
+++ event/src/main/scala/persistence/ConfigEmitterPersistence.scala
@@ -1,21 +1,20 @@
 package persistence
 
 import com.rabbitmq.client.MessageProperties
-
-trait ConfigEmitter {
+import emitter.ConnectionStart._
+trait ConfigEmitterPersistence {
   def start():Unit
   def sendMessage(message:String):Unit
 }
-object ConfigEmitter {
-  def apply(routingKey:String): ConfigEmitter = new ConfigEmitterImpl(routingKey)
-  private class ConfigEmitterImpl(routingKey:String) extends ConfigEmitter{
-    import ConnectionStart._
+object ConfigEmitterPersistence {
+  def apply(routingKey:String): ConfigEmitterPersistence = new ConfigEmitterImpl(routingKey)
+  private class ConfigEmitterImpl(routingKey:String) extends ConfigEmitterPersistence{
     val EXCHANGE_NAME = "info_algorithm_logs"
     val NAME_QUEUE = "licenzia5"
     override def start(): Unit = {
       channel.exchangeDeclare(EXCHANGE_NAME, "topic",true)
-      val queueNameComentario = channel.queueDeclare(NAME_QUEUE, true, false,  false,  null).getQueue;
-      channel.queueBind(queueNameComentario, EXCHANGE_NAME, routingKey);
+      channel.queueDeclare(NAME_QUEUE, true, false,  false,  null).getQueue
+      channel.queueBind(NAME_QUEUE, EXCHANGE_NAME, routingKey)
     }
     override def sendMessage(message:String): Unit ={
       channel.basicPublish(EXCHANGE_NAME, routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"))
@@ -24,8 +23,9 @@ object ConfigEmitter {
 
   }
 }
+
 object das extends App{
-  val s = ConfigEmitter("assumi")
+  val s = ConfigEmitterPersistence("assumi")
   s.start()
   s.sendMessage("3")
 }
--- event/src/main/scala/persistence/ConfigReceiverPersistence.scala
+++ event/src/main/scala/persistence/ConfigReceiverPersistence.scala
@@ -1,25 +1,22 @@
 package persistence
 
 import com.rabbitmq.client.{CancelCallback, DeliverCallback}
-object Common{
-  var nameQueue:String=""
-}
-trait ConfigReceiver{
+trait ConfigReceiverPersistence{
   def start():Unit
   def receiveMessage(method:String=>Unit):Unit
 }
-object ConfigReceiver {
-  def apply(routingKey:String,nameQueue:String): ConfigReceiver = new ConfigReceiverImpl(routingKey,nameQueue)
-  private class ConfigReceiverImpl(routingKey:String,nameQueue:String) extends ConfigReceiver{
+object ConfigReceiverPersistence {
+  def apply(routingKey:String,nameQueue:String): ConfigReceiverPersistence = new ConfigReceiverImpl(routingKey,nameQueue)
+  private class ConfigReceiverImpl(routingKey:String,nameQueue:String) extends ConfigReceiverPersistence{
 
     import emitter.ConnectionStart._
     val EXCHANGE_NAME = "info_algorithm_logs"
     val NAME_QUEUE: String = nameQueue
     override def start(): Unit = {
       channel.exchangeDeclare(EXCHANGE_NAME, "topic",true)
-      channel.queueDeclare(NAME_QUEUE, false, false,  false,  null).getQueue;
-      channel.queueBind(NAME_QUEUE, EXCHANGE_NAME, routingKey);
-      channel.basicQos(0,1,false);
+      channel.queueDeclare(NAME_QUEUE, false, false,  false,  null).getQueue
+      channel.queueBind(NAME_QUEUE, EXCHANGE_NAME, routingKey)
+      channel.basicQos(0,1,false)
     }
 
     override def receiveMessage(method:String=>Unit): Unit ={
@@ -36,19 +33,19 @@ object ConfigReceiver {
 }
 object ssa extends App{
   def g(s:String):Unit = println(s)
-  val e = ConfigReceiver("assumi","licenzia")
+  val e = ConfigReceiverPersistence("assumi","licenzia")
   e.start()
   e.receiveMessage(g)
 }
 object ssa1 extends App{
   def g(s:String):Unit = println(s)
-  val e = ConfigReceiver("assumi","licenzia2")
+  val e = ConfigReceiverPersistence("assumi","licenzia2")
   e.start()
   e.receiveMessage(g)
 }
 object ssa2 extends App{
   def g(s:String):Unit = println(s)
-  val e = ConfigReceiver("assumi","licenzia3")
+  val e = ConfigReceiverPersistence("assumi","licenzia3")
   e.start()
   e.receiveMessage(g)
 }
