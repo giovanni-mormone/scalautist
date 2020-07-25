044cb840a87c1ba99a2c6c2681ed7d7fca557da4
Giovanni Mormone <giovanni.mormone@studio.unibo.it>
2020-07-25 04:00:28 +0200

    added css and fixed layout bugs

---
+++ client/src/main/scala/view/fxview/component/manager/subcomponent/DayInWeekModalLabels.scala
@@ -0,0 +1,39 @@
+package view.fxview.component.manager.subcomponent
+
+import java.net.URL
+import java.util.ResourceBundle
+
+import javafx.fxml.FXML
+import javafx.scene.control.Label
+import view.fxview.component.{AbstractComponent, Component}
+import view.fxview.component.manager.subcomponent.parent.ModalParamParent
+
+trait DayInWeekModalLabels extends Component[ModalParamParent]
+
+
+
+
+object DayInWeekModalLabels{
+
+  def apply(day:String,shift:String,variation:String): DayInWeekModalLabels = new DayInWeekModalLabelsFX(day,shift,variation)
+
+  private class DayInWeekModalLabelsFX(day:String,shift:String,variation:String) extends AbstractComponent[ModalParamParent]("manager/subcomponent/ThreeFieldBox")
+    with DayInWeekModalLabels{
+
+    @FXML
+    var field1: Label = _
+    @FXML
+    var field2: Label = _
+    @FXML
+    var field3: Label = _
+
+    override def initialize(location: URL, resources: ResourceBundle): Unit = {
+      super.initialize(location, resources)
+      field1.setText(day)
+      field2.setText(shift)
+      field3.setText(variation)
+
+    }
+  }
+}
+
