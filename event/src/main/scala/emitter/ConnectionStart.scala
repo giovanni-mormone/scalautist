10651622f280ce9af296632a2fcb4eb09ad30037
faspeeencina <fabian.aspeeencina@studio.unibo.it>
2020-07-30 00:08:09 +0200

    resolved problem oh my god

--- event/src/main/scala/emitter/ConnectionStart.scala
+++ event/src/main/scala/emitter/ConnectionStart.scala
@@ -4,7 +4,8 @@ import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
 
 object ConnectionStart {
   val factory = new ConnectionFactory
-  factory.setHost("localhost")
+  factory.setHost("scalautistrabbit.westeurope.azurecontainer.io")
+  factory.setPort(5672)
   val connection: Connection = factory.newConnection
   val channel: Channel = connection.createChannel
 
