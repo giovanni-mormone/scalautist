8466ae76f47059138a0ac5e3123569b402fc37a3
faspeeencina <fabian.aspeeencina@studio.unibo.it>
2020-08-25 14:35:08 +0200

    change url from new database and web server

--- event/src/main/scala/emitter/ConnectionStart.scala
+++ event/src/main/scala/emitter/ConnectionStart.scala
@@ -4,7 +4,7 @@ import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
 
 object ConnectionStart {
   val factory = new ConnectionFactory
-  factory.setHost("scalautistrabbit.westeurope.azurecontainer.io")
+  factory.setHost("40.114.191.202")
   factory.setPort(5672)
   val connection: Connection = factory.newConnection
   val channel: Channel = connection.createChannel
