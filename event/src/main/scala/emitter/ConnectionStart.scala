87da7f67946bad2aec0563115c6defcff9866e33
Giovanni Mormone <giovanni.mormone@studio.unibo.it>
2020-09-03 10:29:51 +0200

    fixed rabbit ip

--- event/src/main/scala/emitter/ConnectionStart.scala
+++ event/src/main/scala/emitter/ConnectionStart.scala
@@ -4,7 +4,7 @@ import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
 
 object ConnectionStart {
   val factory = new ConnectionFactory
-  factory.setHost("40.114.191.202")
+  factory.setHost("20.54.217.161")
   factory.setPort(5672)
   val connection: Connection = factory.newConnection
   val channel: Channel = connection.createChannel
